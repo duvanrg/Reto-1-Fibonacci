0a35c13057c7ce197d82a5389500edd90a818c89
Duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-12-04 08:41:44 -0500

    feat: :sparkles: Add exercises
    add exercises 1, 2, 3 for Fibonacci
    
    @duvanrg

---
+++ SerieFibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using SerieFibonacci.entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Fibonacci ejercicios = new Fibonacci();
+        // var result = "";
+        bool run = true;
+        while (run)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Entregar el n-ésimo numero");
+            Console.WriteLine("2. Comprobar numero");
+            Console.WriteLine("3. Entregar n numeros de fibonacci");
+            Console.WriteLine("4. Salir");
+            Console.Write("> ");
+            byte opc = Convert.ToByte(Console.ReadLine());
+            Console.Clear();
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine($"Digite el numero de entrada para la serie de fibonacci: ");
+                    Console.Write("> ");
+                    ejercicios.Ejercicio_1(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 2:
+                    Console.WriteLine($"Digite el numero a comprobar en la serie de fibonacci: ");
+                    Console.Write("> ");
+                    ejercicios.Ejercicio_2(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 3:
+                    Console.WriteLine($"Digite la cantidad de numeros que desea recibir de la serie de fibonacci: ");
+                    Console.Write("> ");
+                    ejercicios.Ejercicio_3(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                default:
+                    run = false;
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerieFibonacci/entities/Fibonacci.cs
@@ -0,0 +1,75 @@
+using System.Numerics;
+
+namespace SerieFibonacci.entities
+{
+    public class Fibonacci
+    {
+        public void SerieNormal()
+        {
+            BigInteger num_1 = 0;
+            BigInteger num_2 = 1;
+            for (int i = 0; i < 19; i++)
+            {
+                Console.WriteLine($"{num_1}");
+                BigInteger aux = num_1;
+                num_1 = num_1 + num_2;
+                num_2 = aux;
+            }
+        }
+        public void Ejercicio_1(int n)
+        {
+            BigInteger num_1 = 0;
+            BigInteger num_2 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                BigInteger aux = num_1;
+                num_1 = num_1 + num_2;
+                num_2 = aux;
+            }
+            Console.WriteLine($"el numero {n} correspondiente en la serie de fibonacci es: {num_1}");
+        }
+        public void Ejercicio_2(int n)
+        {
+            BigInteger num_1 = 0;
+            BigInteger num_2 = 1;
+            bool verificar = true;
+            bool run = true;
+            while (run)
+            {
+                if (num_1 == n)
+                {
+                    break;
+                }
+                else if (n < num_1)
+                {
+                    verificar = false;
+                    break;
+                }
+                BigInteger aux = num_1;
+                num_1 = num_1 + num_2;
+                num_2 = aux;
+            }
+            if (verificar)
+            {
+                Console.WriteLine($"{n} es un numero de Fibonacci");
+            }
+            else
+            {
+                Console.WriteLine($"{n} NO es un numero de Fibonacci");
+            }
+        }
+        public void Ejercicio_3(int n)
+        {
+            BigInteger num_1 = 0;
+            BigInteger num_2 = 1;
+            Console.WriteLine($"los {n} primeros numeros de Fibonacci son:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{num_1}  ");
+                BigInteger aux = num_1;
+                num_1 = num_1 + num_2;
+                num_2 = aux;
+            }
+        }
+    }
+}
